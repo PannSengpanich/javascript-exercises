dc582e1988d41a192eb3eabb0f09f185085048f3
Pann Sengpanich <panncraftyo@gmail.com>
2023-03-22 23:10:07 +0700

    complete 08,09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  let res = 0;
+  for (let num of a) {
+    res += num;
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  let res = 1;
+  for (let num of a) {
+    res *= num;
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) {
+    return 1;
+  } else {
+    let res = 1;
+    for (let i = a; i > 0; i--) {
+      res *= i;
+    }
+    return res;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let newstr = str
+    .toLowerCase()
+    .match(/[A-Za-z0-9]/g)
+    .toString();
+  let twinstr = [];
 
+  newstr = newstr.split("");
+  for (let i = 0; i < newstr.length; i++) {
+    twinstr.push(newstr[newstr.length - 1 - i]);
+  }
+  newstr = newstr.join("");
+  twinstr = twinstr.join("");
+  return newstr == twinstr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
